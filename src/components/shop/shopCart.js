f3bd7db355c3cb1844c5d0958ac72f0cd40ffe62
Gina <gkjarvis@mail.fhsu.edu>
2022-01-10 17:22:19 -0700

    added the products component to the cart component

--- src/components/shop/shopCart.js
+++ src/components/shop/shopCart.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import CartProduct from './cartProduct';
+
 
 function CartButton({className, icon}) {
     return (
@@ -10,8 +12,8 @@ function CartButton({className, icon}) {
 
 function CartContent({ className, products }) {
     let count = products.length;
-    let productsJSX = products.map(product => <h1 key={product} > {product} </h1>);
-    return (
+    let productsJSX = products.map(product => <CartProduct key={product} />); 
+    return(
         <div className={`${className} cart-content`}>
             <div className='cart-content__title'>
                 Cart ({count})
