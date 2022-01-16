a1bf41546531c1886bd45b188e12ff477b937318
Gina <gkjarvis@mail.fhsu.edu>
2022-01-16 16:17:22 -0700

    added order subtotal and total functionality

--- src/components/order/review.js
+++ src/components/order/review.js
@@ -15,15 +15,25 @@ class Review extends Component {
         console.log('fields');
     } 
     render() {
+        let subtotal = 0;
+        this.props.cartProducts.map(cartProduct => {
+            subtotal += cartProduct.quantity * cartProduct.product.price;
+        })
         return (
             <div className='review'>
                 <PageTitle className='review__page-title' title='Order Review' />
-                <ReviewForm className='review__form' onSubmit={this.onSubmit}/>
+                <ReviewForm className='review__form' onSubmit={this.onSubmit} subtotal={ subtotal}/>
             </div>
         )
     }
 }
+function mapStateToProps(state) {
+    const { cartProducts } = state.user;
+    return { cartProducts }
+}
 
-Review = connect(null, actions)(Review);
+Review = connect(mapStateToProps, actions)(Review);
+
+
 
 export default Review; 
